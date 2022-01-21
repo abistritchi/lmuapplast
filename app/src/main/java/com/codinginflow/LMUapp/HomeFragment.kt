c587bba93c622a104a0c630b85c9ef972f027d7e
ru63qov@unimuenchen.onmicrosoft.com <ru63qov@unimuenchen.onmicrosoft.com>
2022-01-21 18:58:49 +0100

    home fragment -> auto changeable image

--- app/src/main/java/com/codinginflow/LMUapp/HomeFragment.kt
+++ app/src/main/java/com/codinginflow/LMUapp/HomeFragment.kt
@@ -5,12 +5,38 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_search.*
+import android.os.CountDownTimer
+import android.widget.TextView
+import com.codinginflow.LMUapp.databinding.FragmentHomeToWebBinding
+import kotlin.system.measureTimeMillis
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        object : CountDownTimer(1000000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val s = millisUntilFinished/10000
+                    if ((s % 10).mod(2)==0){
+                        button_weather_home.setImageResource(R.drawable.weather)
+                        SearchFragment.web_adress = "https://www.accuweather.com/en/de/munich/80331/weather-forecast/178086"
+                    }
+                    if ((s % 10).mod(2)!=0) {
+                        button_weather_home.setImageResource(R.drawable.lmu_building)
+                        SearchFragment.web_adress = "https://www.lmu.de/de/index.html"
+                }
+
+
+            }
+
+            override fun onFinish() {
+                    button_weather_home.setImageResource(R.drawable.weather)
+                    SearchFragment.web_adress = "https://www.accuweather.com/en/de/munich/80331/weather-forecast/178086"
+            }
+        }.start()
 
         button_uni2work.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeFragmentToUni2work()
@@ -74,6 +100,11 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         }
 
 
+        button_weather_home.setOnClickListener {
+            //SearchFragment.web_adress = "https://www.accuweather.com/en/de/munich/80331/weather-forecast/178086"
+            val action = HomeFragmentDirections.actionHomeFragmentToHomeToWeb2()
+            findNavController().navigate(action)
+        }
 
 
 
