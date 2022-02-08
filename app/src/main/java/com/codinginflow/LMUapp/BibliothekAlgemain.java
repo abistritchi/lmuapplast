f95c5edca501e1d80e0e4409e2bf22bef98ee84a
ru63qov@unimuenchen.onmicrosoft.com <ru63qov@unimuenchen.onmicrosoft.com>
2022-02-08 21:07:01 +0100

    new site design

--- app/src/main/java/com/codinginflow/LMUapp/BibliothekAlgemain.java
+++ app/src/main/java/com/codinginflow/LMUapp/BibliothekAlgemain.java
@@ -28,9 +28,6 @@ public class BibliothekAlgemain extends AppCompatActivity implements OnMapReadyC
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.biblothek_algemain);
-
-        setupData();
-        setUpList1();
         setUpOnclickListener1();
         
 
@@ -47,61 +44,9 @@ public class BibliothekAlgemain extends AppCompatActivity implements OnMapReadyC
 
     }
 
-    private void setupData()
-    {
-
-        shapeList.clear();
-        Shape circle = new Shape("0", "Bibliothek 1", R.drawable.news1);
-        shapeList.add(circle);
-
-        Shape triangle = new Shape("1","Bibliothek 2", R.drawable.news3);
-        shapeList.add(triangle);
-
-        Shape square = new Shape("2","Bibliothek 3", R.drawable.news2);
-        shapeList.add(square);
-
-        Shape rectangle = new Shape("3","Bibliothek 4", R.drawable.news2);
-        shapeList.add(rectangle);
-
-        Shape octagon = new Shape("4","Bibliothek 5", R.drawable.news2);
-        shapeList.add(octagon);
-
-        Shape circle2 = new Shape("5", "Bibliothek 6", R.drawable.news1);
-        shapeList.add(circle2);
-
-        Shape triangle2 = new Shape("6","Bibliothek 7", R.drawable.news3);
-        shapeList.add(triangle2);
-
-        Shape square2 = new Shape("7","Bibliothek 8", R.drawable.news2);
-        shapeList.add(square2);
-
-        Shape rectangle2 = new Shape("8","Bibliothek 9", R.drawable.news2);
-        shapeList.add(rectangle2);
-
-        Shape octagon2 = new Shape("9","Bibliothek 10", R.drawable.news2);
-        shapeList.add(octagon2);
-    }
-
-    private void setUpList1()
-    {
-        listView = (ListView) findViewById(R.id.shapesListView1);
-
-        ShapeAdapter adapter = new ShapeAdapter(getApplicationContext(), 0, shapeList);
-        listView.setAdapter(adapter);
-    }
 
     private void setUpOnclickListener1()
     {
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
-            {
-                Shape selectShape = (Shape) (listView.getItemAtPosition(position));
-                Intent showDetail = new Intent(getApplicationContext(), DetailActivity.class);
-                showDetail.putExtra("id",selectShape.getId());
-                startActivity(showDetail);
-            }
-        });
 
         //Get a place at the bibliothek
         final Button registration = findViewById(R.id.bibliothek_platz_regiestrierung2);
